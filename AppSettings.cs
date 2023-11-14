ae7ff39d1850cdffad116c85ec1dfe90740d5561
Pradeep <pradeepbrh@gmail.com>
2023-11-14 13:01:18 +0530

    add all files

---
+++ AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageApp
+{
+    public class AppSettings
+    {
+        public string ApiKey { get; set; }
+        public string DatabaseConnectionString { get; set; }
+        public string LogLevel { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static void Main()
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        string apiKey = configuration["AppSettings:OutputPath"];
+        
+
+        Console.WriteLine($"ApiKey: {apiKey}");
+
+        Console.ReadLine();
+        // Rest of your console application logic
+    }
+}
